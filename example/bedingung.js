a452b4de8dbf78d8c492826d0577f2527532e866
tinoschroeter <mail@wgnetworks.de>
2020-02-22 17:30:41 +0100

    update if cases

--- example/bedingung.js
+++ example/bedingung.js
@@ -2,6 +2,8 @@ if(1 < 4 && 2 < 9) {
     console.log('1 < 4 und 2 < 9')
 } else if(8 > 7 || 3 == 4) {
     console.lgo('8 > 7 oder 3 ist gleich 4');
-} else {
+} else if (a != b) {
+    console.log('a ist ungleich b')
+}else {
     console.log('keine Bedingung erfuellt');
 }
